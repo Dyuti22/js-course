7f33e504d6b288737e93f0c48a26278b54513b45
Dyuti22 <127475733+Dyuti22@users.noreply.github.com>
2024-02-14 17:08:27 +0000

    strings & nums and maths

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,7 +5,7 @@ console.log(name + repoCount + " Value"); //not recommended
 
 console.log(`Hello my my name is ${name} and my repo-count is ${repoCount}`);
 
-const gameName = new String('Dyuti');
+const gameName = new String('Dyuti-Sharma');
 /*String {'Dyuti'} //as key-value pairs not array
 0: "D"
 1: "y"
@@ -20,3 +20,20 @@ console.log(gameName[0]);
 console.log(gameName.__proto__)
 
 console.log(gameName.length); 
+
+const newString = gameName.substring(0,4); //only positive values
+console.log(newString)
+
+const newString2 = gameName.slice(-8,2); //both +ve & -ve values
+console.log(newString2)
+
+const newString3 = "          Dyuti         "
+console.log(newString3)
+console.log(newString3.trim()) //trims blank spaces
+
+const url = "https://dyutified.com/dyuti%20sharma" 
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('dyuti'))
+
+console.log(gameName.split('-'));
---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,21 @@
+const score = 400
+//console.log(score);
+const balance = new Number(100);
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2)); //100.00
+
+const otherNumber = 1123.8966
+//console.log(otherNumber.toFixed(1)); //23.9
+//console.log(otherNumber.toPrecision(3)) //1.12e+3
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString());//1,000,000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4)) //4
+console.log(Math.round(4.768)) //5
